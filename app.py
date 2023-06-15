128891812f24b5eebcc509b4bdb4c58096782de9
Son Den <115443243+RoadToDev101@users.noreply.github.com>
2023-06-15 15:06:43 +0700

    Add error handler to api

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import requests
 import streamlit as st
 import pandas as pd
 from pydantic import BaseModel
+from sklearn.preprocessing import RobustScaler
+import numpy as np
 
 
 class Data(BaseModel):
@@ -12,45 +14,57 @@ def make_prediction(data_frame: pd.DataFrame):
     # Define the API endpoint for the FastAPI backend
     api_endpoint = "http://localhost:5001/api/predict/test"  # Modify the URL if needed
 
+    # Load the scaler
+    numeric_features = list(
+        data_frame.select_dtypes(include=[np.number]).columns.values
+    )
+
+    scaler = RobustScaler()
+
+    for col in numeric_features:
+        data_frame[[col]] = scaler.fit_transform(data_frame[[col]])
+
     # Convert dataFrame to dummy variables
     data = pd.get_dummies(data_frame)
-    # return data
 
     # Prepare the request data
     request_data = {
         "data": data.values.tolist()
     }  # Convert the DataFrame to a nested list
 
-    # Send a POST request to the API endpoint
-    response = requests.post(api_endpoint, json=request_data)
-
-    # Parse the response as JSON
-    predictions = response.json()["predictions"]
-
-    return predictions
+    try:
+        # Send a POST request to the API endpoint
+        response = requests.post(api_endpoint, json=request_data)
+        response.raise_for_status()  # Raise an exception for HTTP errors
+        # Parse the response as JSON
+        predictions = response.json()["predictions"]
+        return predictions
+    except requests.exceptions.RequestException as e:
+        st.error(f"Error occurred: {e}")
+        return None
 
 
 def main():
-    st.title("Predictive App")
-
-    major_diameter = st.number_input("Major Diameter", value=0.0)
-    minor_diameter = st.number_input("Minor Diameter", value=0.0)
-    thread_pitch = st.number_input("Thread Pitch", value=0.0)
+    st.title("Anchor Installation Time Predict")
+    st.header("Input Parameters")
+    major_diameter = st.number_input("Major Diameter (in.)", value=0.0)
+    minor_diameter = st.number_input("Minor Diameter (in.)", value=0.0)
+    thread_pitch = st.number_input("Thread Pitch (in.)", value=0.0)
     included_angle = st.number_input("Included Angle", value=0.0)
     top_angle = st.number_input("Top Angle", value=0.0)
-    hole_diameter = st.number_input("Hole Diameter", value=0.0)
+    hole_diameter = st.number_input("Hole Diameter (in.)", value=0.0)
     serration_threads = st.radio(
-        "Serration Threads", options=[False, True], index=False
+        "Have Serration Threads", options=[False, True], index=False
     )
     if serration_threads:
         serration_threads = 1
     else:
         serration_threads = 0
-    length_B = st.number_input("Length B", value=0.0)
-    length_C = st.number_input("Length C", value=0.0)
-    embedment_depth = st.number_input("Embedment Depth", value=0.0)
+    length_B = st.number_input("Total length (in.)", value=0.0)
+    length_C = st.number_input("Thread length (in.)", value=0.0)
+    embedment_depth = st.number_input("Embedment Depth (in.)", value=0.0)
     tip_taper = st.number_input("Tip Taper", value=0.0)
-    shank_diameter = st.number_input("Shank Diameter", value=0.0)
+    shank_diameter = st.number_input("Shank Diameter (in.)", value=0.0)
     torque_tool = st.selectbox("Torque Tool", options=["Low", "Medium", "High"])
     if torque_tool == "Low":
         torque_tool = 225
@@ -58,16 +72,13 @@ def main():
         torque_tool = 300
     else:
         torque_tool = 500
-    concrete_tx = st.radio("Texas Concrete", options=[False, True], index=False)
-    if concrete_tx:
+    concrete = st.radio("Concrete", options=["Texas", "West Chicago"])
+    if concrete == "Texas":
         concrete_tx = 1
+        concrete_wc = 0
     else:
         concrete_tx = 0
-    concrete_wc = st.radio("West Chicago Concrete", options=[False, True], index=False)
-    if concrete_wc:
         concrete_wc = 1
-    else:
-        concrete_wc = 0
 
     input = {
         "major_diameter": major_diameter,
@@ -92,10 +103,15 @@ def main():
 
     if st.button("Predict"):
         predictions = make_prediction(data_frame)
-        # st.write(predictions)
-        st.write("Predictions:")
+        st.header("Predictions:")
+
+        # Create a table to display the predictions
+        table_data = []
         for model_name, prediction in predictions.items():
-            st.write(f"{model_name}: {prediction}")
+            table_data.append([model_name, prediction])
+
+        table_df = pd.DataFrame(table_data, columns=["Model", "Prediction"])
+        st.table(table_df)
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -3,6 +3,12 @@ from pydantic import BaseModel
 import joblib
 import numpy as np
 import os
+from error_handler import (
+    BadRequestError,
+    handle_bad_request_error,
+    NotFoundError,
+    handle_not_found_error,
+)
 
 app = FastAPI()
 
@@ -18,23 +24,39 @@ for filename in os.listdir(model_directory):
         model = joblib.load(model_path)
         models[model_name] = model
 
+if not models:
+    raise NotFoundError("Not found any models")
+
 
 class Data(BaseModel):
     data: list
 
 
-@app.post("/api/predict/test")  # Modify the API endpoint if needed
+@app.exception_handler(BadRequestError)
+async def handle_bad_request(request, exc):
+    return await handle_bad_request_error(request, exc)
+
+
+@app.exception_handler(NotFoundError)
+async def handle_not_found(request, exc):
+    return await handle_not_found_error(request, exc)
+
+
+@app.post("/api/predict/test")
 def predict(data: Data):
-    # Reshape the input data
-    X = np.array(data.data).reshape(1, -1)
+    try:
+        # Reshape the input data
+        X = np.array(data.data).reshape(1, -1)
 
-    # Predict and store the results for each model
-    predictions = {}
-    for model_name, model in models.items():
-        prediction = model.predict(X)
-        prediction = np.expm1(prediction)
-        predictions[model_name] = prediction.tolist()
+        # Predict and store the results for each model
+        predictions = {}
+        for model_name, model in models.items():
+            prediction = model.predict(X)
+            prediction = np.expm1(prediction)
+            predictions[model_name] = f"{prediction[0]:.6f} seconds"
 
-    # Format the prediction response as JSON
-    response = {"predictions": predictions}
-    return response
+        # Format the prediction response as JSON
+        response = {"predictions": predictions}
+        return response
+    except Exception as e:
+        raise BadRequestError("Invalid request data.")
